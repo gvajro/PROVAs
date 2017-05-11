f0a1aac6ce3296abdfe8b96c27fddb9f930764bf
gvajro <giuseppe.vajro@gmail.com>
2017-05-11 22:06:09 +0200

    contatti

--- index.php
+++ index.php
@@ -14,52 +14,7 @@
           include("include/header.php");
         ?>
 
-    <nav  data-spy="affix" data-offset-top="60" data-offset-bottom="200" id="navbar" class="navbar-default">
-      <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"><img src="image/logo.svg" alt=""></a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#chisiamo">Chi Siamo</a></li>
-            <li><a href="prodotti.html">Prodotti</a></li>
-            <li><a href="#contatti">Contatti</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ricette <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Le novità</a></li>
-                <li><a href="#">Le preferite</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">Video ricette</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
-
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-hidden-xs col-sm-12">
-          <a href="#"><div class="hero__image">
-            <div class="hero__image--text">
-              <h1>La vita è una combinazione di pasta e magia.</h1>
-              <h2>Federico Fellini</h2>
-            </div>
-          </div></a>
-        </div>
-      </div>
-    </div>
-
+    
 
   <div class="container">
     <div class="row">
